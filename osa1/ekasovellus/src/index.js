f1bcf2e6aa72ccbcf81f230b3d8e5ff4ce925561
Leppänen Riku <Riku.Leppanen@almamedia.fi>
2018-01-17 10:30:30 +0200

    Tehtava1.2

--- osa1/ekasovellus/src/index.js
+++ osa1/ekasovellus/src/index.js
@@ -10,10 +10,11 @@ const App = () => {
     const osa3 = 'Komponenttien tila'
     const tehtavia3 = 14
 
+
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3}/>
+            <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3} />
             <Yhteensa summa={tehtavia1 + tehtavia2 + tehtavia3} />
         </div>
     )
@@ -25,12 +26,20 @@ const Yhteensa = (props) => {
         </div>
     )
 }
+
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa} {props.tehtava}</p>
+        </div>
+    )
+}
 const Sisalto = (props) => {
     return (
         <div>
-            <p>{props.osa1} {props.tehtavia1}</p>
-            <p>{props.osa2} {props.tehtavia2}</p>
-            <p>{props.osa3} {props.tehtavia3}</p>
+            <Osa osa={props.osa1} tehtava={props.tehtavia1} />
+            <Osa osa={props.osa2} tehtava={props.tehtavia2} />
+            <Osa osa={props.osa3} tehtava={props.tehtavia3} />
         </div>
     )
 }
