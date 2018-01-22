94a18743faa567dacec88f5a0d5663f8697d89ba
Leppänen Riku <Riku.Leppanen@almamedia.fi>
2018-01-22 16:07:23 +0200

    Osa2.Tehtävä2.11

--- osa2/pluettelo/src/App.js
+++ osa2/pluettelo/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Item from './components/Item'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       search: ''
     }
+  }
+  componentWillMount() {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      this.setState({persons: response.data})
+    })
   }
 
   addItem = (event) => {
